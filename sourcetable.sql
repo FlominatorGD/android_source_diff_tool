d8e621e097cb72d4c089088527c8357c12e5638b
Flominator <github.flominatorgd@gmail.com>
2024-06-20 18:21:53 +0200

    fix unique table names sourcetable.sql

--- sourcetable.sql
+++ sourcetable.sql
@@ -8,48 +8,48 @@ CREATE DATABASE sourcetable;
 USE sourcetable;
 
 -- Create Source table
-CREATE TABLE Source (
+CREATE TABLE Source_t (
     SourceID INT PRIMARY KEY,
     Name VARCHAR(255) UNIQUE
 );
 
 -- Create Folder table
-CREATE TABLE Folder (
+CREATE TABLE Folder_t (
     FolderID INT PRIMARY KEY,
     Name VARCHAR(255) UNIQUE,
     SourceID INT,
-    FOREIGN KEY (SourceID) REFERENCES Source(SourceID)
+    FOREIGN KEY (SourceID) REFERENCES Source_t(SourceID)
 );
 
 -- Create File table
-CREATE TABLE File (
+CREATE TABLE File_t (
     FileID INT PRIMARY KEY,
     Name VARCHAR(255) UNIQUE,
     Type VARCHAR(50),
     FolderID INT,
-    FOREIGN KEY (FolderID) REFERENCES Folder(FolderID)
+    FOREIGN KEY (FolderID) REFERENCES Folder_t(FolderID)
 );
 
 -- Create Line table
-CREATE TABLE Line (
+CREATE TABLE Line_t (
     LineID INT PRIMARY KEY,
     Name VARCHAR(255) UNIQUE,
     FileID INT,
-    FOREIGN KEY (FileID) REFERENCES File(FileID)
+    FOREIGN KEY (FileID) REFERENCES File_t(FileID)
 );
 
 -- Create Colum table
-CREATE TABLE Column (
+CREATE TABLE Column_t (
     ColumnID INT PRIMARY KEY,
     Name VARCHAR(255) UNIQUE,
     LineID INT,
-    FOREIGN KEY (LineID) REFERENCES Line(LineID)
+    FOREIGN KEY (LineID) REFERENCES Line_t(LineID)
 );
 
 -- Create Character table
-CREATE TABLE Character (
+CREATE TABLE Character_t (
     CharacterID INT PRIMARY KEY,
     Value CHAR(1),
     LineID INT,
-    FOREIGN KEY (LineID) REFERENCES Line(LineID)
+    FOREIGN KEY (LineID) REFERENCES Line_t(LineID)
 );
